08a13dc78e38d1ea293b2e87ff1cc8454c8cff29
avi028 <math28arnav@gmail.com>
2022-07-15 10:54:38 +0530

    added some css to the testWebsite

--- includes.h
+++ includes.h
@@ -1,21 +1,26 @@
-#include <iostream>
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/sendfile.h>
-#include <netdb.h>
-#include <string.h> 
-#include <arpa/inet.h>
-#include <string>
-#include <iterator>
-#include <sstream>
-#include <streambuf>
-#include <vector>
-#include <fstream>
-#include <map>
-#include <stdlib.h>
-#include <time.h>
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+    #include <iostream>
+    #include <stdio.h>
+    #include <fcntl.h>
+    #include <stdlib.h>
+    #include <sys/types.h>
+    #include <unistd.h>
+    #include <sys/socket.h>
+    #include <sys/stat.h>
+    #include <sys/sendfile.h>
+    #include <netdb.h>
+    #include <string.h> 
+    #include <arpa/inet.h>
+    #include <string>
+    #include <iterator>
+    #include <sstream>
+    #include <streambuf>
+    #include <vector>
+    #include <fstream>
+    #include <map>
+    #include <stdlib.h>
+    #include <time.h>
+
+#endif
